9af8cee238eb0c215b26a8df0525e4a4c1bfdc4d
gdaumer24 <geoffroy.daumer@outlook.fr>
2025-02-20 17:48:44 +0100

    maj

--- front/src/app/features/themes/themes/themes.component.ts
+++ front/src/app/features/themes/themes/themes.component.ts
@@ -1,12 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { BanneerConnectedComponent } from '../../../shared/banneer-connected/banneer-connected.component';
 import { ThemeService } from '../services/theme.service';
+import { MatCardModule } from '@angular/material/card';
+import { AsyncPipe, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-themes',
-  imports: [BanneerConnectedComponent],
+  standalone: true,
+  imports: [BanneerConnectedComponent, MatCardModule, NgFor, AsyncPipe],
   templateUrl: './themes.component.html',
-  styleUrl: './themes.component.css',
+  styleUrls: ['./themes.component.css'],
 })
 export class ThemesComponent {
   private themeService = inject(ThemeService);
