6286e5c16efd39498fdf15f8c00d5f016804d1f4
gdaumer24 <geoffroy.daumer@outlook.fr>
2025-02-27 21:52:41 +0100

    debut spring security

--- back/src/main/java/com/network/security/WebSecurityConfig.java
+++ back/src/main/java/com/network/security/WebSecurityConfig.java
@@ -1,19 +1,80 @@
 package com.network.security;
 
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+
 
 /**
  * Congif for security
  */
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
     
+  @Bean
+  public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+    PasswordEncoder passwordEncoder = passwordEncoder();
+    return new InMemoryUserDetailsManager(
+      User.withUsername("user1")
+        .password(passwordEncoder.encode("12345"))
+        .authorities("USER")
+        .build(),
+      User.withUsername("admin")
+        .password(passwordEncoder.encode("12345"))
+        .authorities("USER", "ADMIN")
+        .build()
+    );
+  }
+
      @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
 
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    return httpSecurity
+      .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+      .csrf(csrf -> csrf.disable())
+      .authorizeHttpRequests(ar -> ar.anyRequest().authenticated())
+      //.httpBasic(Customizer.withDefaults())
+      .oauth2ResourceServer(oa -> oa.jwt(Customizer.withDefaults()))
+      .build();
+  } 
+
+  @Value("${jwt.secret}")
+  private String secretKey;
+
+  @Value("${jwt.expiration-time}")
+  private long jwtExpiration;
+
+  @Bean
+  JwtEncoder jwtEncoder() {
+    return new NimbusJwtEncoder(new ImmutableSecret<>(secretKey.getBytes()));
+  }
+
+  @Bean
+  JwtDecoder jwtDecoder() {
+    SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), "RSA");
+    return NimbusJwtDecoder.withSecretKey(secretKeySpec).macAlgorithm(MacAlgorithm.HS512).build();
+  }
 }
