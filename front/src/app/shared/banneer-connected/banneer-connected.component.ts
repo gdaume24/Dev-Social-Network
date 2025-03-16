d70432e00edbcba479a31da10cca59b5a5f0e74b
gdaumer24 <geoffroy.daumer@outlook.fr>
2025-03-16 21:38:06 +0100

    pas trop mal la

--- front/src/app/shared/banneer-connected/banneer-connected.component.ts
+++ front/src/app/shared/banneer-connected/banneer-connected.component.ts
@@ -2,21 +2,25 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { NavigationStart, Router } from '@angular/router';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-banneer-connected',
-  imports: [MatIconModule, MatMenuModule],
+  imports: [MatIconModule, MatMenuModule, MatSidenavModule, FormsModule],
   templateUrl: './banneer-connected.component.html',
   styleUrl: './banneer-connected.component.css',
 })
 export class BanneerConnectedComponent {
   themeRoute = false;
   articleRoute = false;
+  isToggled: boolean = false;
   constructor(private router: Router) {}
   ngOnInit(): void {
     this.themeRoute = this.router?.url === '/themes';
     this.articleRoute = this.router?.url === '/articles';
   }
+
   navigateToThemes() {
     this.router.navigate(['/themes']);
   }
@@ -33,6 +37,12 @@ export class BanneerConnectedComponent {
 
     // Redirect to login or home page
     this.router.navigate(['']);
+  }
+  toggle() {
+    this.isToggled = !this.isToggled;
+  }
+  closeToggle() {
+    this.isToggled = false;
   }
 
 }
