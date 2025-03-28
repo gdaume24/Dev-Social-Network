9dbda49f09af6f14cc492c90e0d2dd446bec7c8c
gdaumer24 <geoffroy.daumer@outlook.fr>
2025-03-28 18:00:51 +0100

    maj

--- front/src/app/features/articles/pages/articles/articles.component.ts
+++ front/src/app/features/articles/pages/articles/articles.component.ts
@@ -1,20 +1,23 @@
 import { Component, inject } from '@angular/core';
-import { ArticleComponent } from '../../components/article/article.component';
 import { BanneerConnectedComponent } from '../../../../shared/banneer-connected/banneer-connected.component';
 import { Router } from '@angular/router';
 import { ArticleService } from '../../service/article.service';
 import { Article } from '../../interface/article.interface';
-import { NgFor, DatePipe, CommonModule } from '@angular/common';
+import { NgFor, CommonModule, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-articles',
-  imports: [BanneerConnectedComponent, NgFor, CommonModule],
+  imports: [BanneerConnectedComponent, NgFor, CommonModule, NgIf],
   templateUrl: './articles.component.html',
   styleUrl: './articles.component.css',
 })
 export class ArticlesComponent {
   articleService = inject(ArticleService);
   articles: Article[] = [];
+  sortOptions = ['date', 'titre']; // Options de tri
+  currentSort: string | null = null;
+  sortAscending = true; // Indique si le tri est croissant ou décroissant
+  isDropdownOpen = false; // État du menu déroulant
 
   constructor(private router: Router) {}
 
@@ -30,5 +33,38 @@ export class ArticlesComponent {
 
   goToCreateArticle() {
     this.router.navigate(['/new-article']);
+  }
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+  sortBy(sortKey: string) {
+    if (this.currentSort === sortKey) {
+      // Même critère : on inverse l'ordre
+      this.sortAscending = !this.sortAscending;
+    } else {
+      // Nouveau critère : ordre ascendant par défaut
+      this.currentSort = sortKey;
+      this.sortAscending = true;
+    }
+
+    this.sortArticles();
+    this.isDropdownOpen = false; // Ferme le dropdown après sélection
+  }
+
+  sortArticles() {
+    if (!this.currentSort) return;
+
+    this.articles.sort((a, b) => {
+      let comparison = 0;
+
+      if (this.currentSort === 'date') {
+        comparison = a.date.getTime() - b.date.getTime();
+      } else if (this.currentSort === 'titre') {
+        comparison = a.title.localeCompare(b.title);
+      }
+
+      // Inverser l'ordre si nécessaire
+      return this.sortAscending ? comparison : -comparison;
+    });
   }
 }
