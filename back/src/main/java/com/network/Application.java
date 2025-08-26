59f93edd1ff09bc3b93fc21450768f878d8a3287
gdaumer24 <geoffroy.daumer@outlook.fr>
2025-08-26 02:17:42 +0200

    Projet restauré pour fonctionner en dev mode

--- back/src/main/java/com/network/Application.java
+++ back/src/main/java/com/network/Application.java
@@ -15,18 +15,13 @@ public class Application {
 
 	public static void main(String[] args) {
 
-		// Ne charger .env que si on est en local (par exemple profil "dev")
-		String activeProfile = System.getenv("ACTIVE_PROFILE");
-
-		if ("dev".equalsIgnoreCase(activeProfile)) {
-			try {
-				Dotenv dotenv = Dotenv.configure().load();
-				dotenv.entries().forEach(entry ->
-						System.setProperty(entry.getKey(), entry.getValue())
-				);
-			} catch (Exception e) {
-				System.err.println("⚠️ Impossible de charger le .env en profil dev : " + e.getMessage());
-			}
+		try {
+			Dotenv dotenv = Dotenv.configure().directory("./back").load();
+			dotenv.entries().forEach(entry ->
+					System.setProperty(entry.getKey(), entry.getValue())
+			);
+		} catch (Exception e) {
+			System.err.println("⚠️ Impossible de charger le .env en profil dev : " + e.getMessage());
 		}
 		SpringApplication.run(Application.class, args);
 	}
